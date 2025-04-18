bbc5d8c2a98eddcde7799150b4a6e7dcd8e2d3c0
bvilla844 <brayanvilla844@gmail.com>
2025-04-17 21:26:09 -0500

    sexto commit - arreglo de avatar para celular

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,13 +10,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=pu=lz=(6ht(jk1q=f)l_=ge1t_wg#%h^_yd545t3%cddxo*d%'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+'''
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+'''
 
 # Application definition
 
